e39627d98dab5289bf589eae93ef45638027efe4
lijianhua04 <lijianhua04@meituan.com>
2019-05-27 11:45:59 +0800

    解决starter打包Unable to find main class

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 
-####自定义Spring boot starter
+#### 自定义Spring boot starter
 
 
 
@@ -8,3 +8,23 @@
 
 - log-spring-boot-starter
 方法加注解，拦截器打印执行时间。
+
+
+<br>
+##### Q&A
+自己实现的自定义Spring boot starter，打包编译时(mvn clean package install)出现：
+```
+Execution default of goal org.springframework.boot:spring-boot-maven-plugin:2.0.6.RELEASE:repackage failed: Unable to find main class -> [Help 1]
+```
+百试不解，最终发现自己引入了spring-boot-maven-plugin：
+```
+<build>
+    <plugins>
+        <plugin>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-maven-plugin</artifactId>
+        </plugin>
+    </plugins>
+</build>
+```
+去除即可。
--- greet-spring-boot-starter/src/main/java/com/ljheee/starter/greet/GreetAutoConfiguration.java
+++ greet-spring-boot-starter/src/main/java/com/ljheee/starter/greet/GreetAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.ljheee.starter.greet;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +18,8 @@ public class GreetAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
-    public Greet greet(){
+    @ConditionalOnProperty(prefix = "spring.greet", name = "name", havingValue = "Jack Lee")// 配置文件 spring.greet.name=Jack Lee时才创建bean；精准匹配，可以使框架精准创建不同的bean
+    public Greet greet() {
         return new GreetImpl();
     }
 
